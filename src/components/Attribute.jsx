cdb2275f536d4e267d3be58e4dd06326fba1d2aa
Odohofre <odohofrebright@gmail.com>
2023-02-01 02:53:45 +0100

    add project files

---
+++ src/components/Attribute.jsx
@@ -0,0 +1,21 @@
+export default function Attribute() {
+  return (
+    <div>
+      <p className=" text-[11px] text-center">
+        Challenge by{" "}
+        <a
+          className="text-attribute"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className="text-attribute" href="https://github.com/Odohofre">
+          Bright Odohofre
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,32 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        attribute: '#3e52a3',
+        primary: {
+          blue: '#5847eb',
+          work: '#ff8c66',
+          play: '#56c2e6',
+          study: '#ff5c7c',
+          exercise: '#4acf81',
+          social: '#7536d3',
+          care: '#f1c65b',
+        },
+        'neutral-blue': {
+          100: '#bdc1ff',
+          200: '#6f76c8',
+          700: '#1c1f4a',
+          800: '#0f1424',
+        }
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"], // font-weights: 300, 400, 500
+      }
+    },
   },
   plugins: [],
 }

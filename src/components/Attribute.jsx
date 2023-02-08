432eb7801021e87320a247d71fc0973895440bc7
Odohofre <odohofrebright@gmail.com>
2023-02-08 23:49:43 +0100

    make the attribute fit one line in small devices

--- src/components/Attribute.jsx
+++ src/components/Attribute.jsx
@@ -1,7 +1,7 @@
 export default function Attribute() {
   return (
-    <footer>
-      <p className="text-[11px] text-center bg-neutral-blue-700 p-4 rounded-md">
+    <footer className="w-full">
+      <p className="text-[11px] text-center bg-neutral-blue-700 py-4 px-2.5 rounded-md">
         Challenge by{" "}
         <a
           className="text-attribute"
